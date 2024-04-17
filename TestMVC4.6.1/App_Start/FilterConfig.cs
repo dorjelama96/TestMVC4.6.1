5c3ea442bda5d99c991b0ab4456f745e228ee8cb
Dorje Lama <dorjelama08@gmail.com>
2024-04-17 16:56:38 +0545

    Add project files.

---
+++ TestMVC4.6.1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TestMVC4._6._1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TestMVC4.6.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestMVC4._6._1.MvcApplication" Language="C#" %>
